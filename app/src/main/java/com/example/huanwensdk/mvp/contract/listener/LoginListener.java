c98cda680e4afd4e3a66ec080957dfb3fae79bf7
ruan3 <1633486734@qq.com>
2018-11-09 14:34:00 +0800

    适配长生缘

--- app/src/main/java/com/example/huanwensdk/mvp/contract/listener/LoginListener.java
+++ app/src/main/java/com/example/huanwensdk/mvp/contract/listener/LoginListener.java
@@ -3,10 +3,10 @@ package com.example.huanwensdk.mvp.contract.listener;
 import com.example.huanwensdk.DataBase.infoUser.User;
 
 public interface LoginListener {
-	
-	void onLogin(User user);
-	
-	void fail(int code,String error); 
+
+	void onLogin(User user,String sign);
+
+	void fail(int code,String error);
 }
   
  
